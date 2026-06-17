94cc37713dfd55a2c1be69e7c3d8596f5ea5fc5b
Dmitriy <tolstyachokhenkov@yandex.ru>
2026-06-18 01:00:48 +0300

    add file index.js and start to describe main logic

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import updateUI from './view.js'
+
+const form = document.querySelector('.form-group')
+const input = document.getElementById('RSS-link')
+const messageField = document.querySelector('.input-message')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+    const content = formData.get('link')
+
+    updateUI(content, input, messageField)
+
+    form.reset()
+    input.focus()
+})
