dd8aebdc9a8710cb974a8f2d630f348c475d17cf
Dmitriy <tolstyachokhenkov@yandex.ru>
2026-06-29 20:39:26 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const form = document.querySelector('.form-group')
 const input = document.getElementById('RSS-link')
 const messageField = document.querySelector('.input-message')
 
+input.focus()
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
